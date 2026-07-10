e7ed240de354b73e8acc7f4f4838b6d3951f2446
Nguyễn Hữu Lộc <lienloc@gmail.com>
2026-07-10 19:16:43 +0700

    Delete baitap3_spring_mongodb_api directory
    Nhầm ghi chú

--- baitap3_spring_mongodb_api/src/main/java/com/nhl/baitap3_spring_mongodb_api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nhl.baitap3_spring_mongodb_api.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException (String message){
-        super(message);     // todo
-    }
-}
